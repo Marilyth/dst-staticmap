28399301901c95d1827e395d8a9f3591ce2a962b
May <baermay98@gmail.com>
2021-01-04 16:57:26 +0100

    Add files

---
+++ modinfo.lua
@@ -0,0 +1,51 @@
+-- This information tells other players more about the mod
+name = "Static Map"
+description = "Essentially makes the map static, by snapping the camera angle whenever the map is toggled."
+author = "Marilyth"
+version = "1.1"
+
+
+-- This lets other players know if your mod is out of date, update it to match the current version in the game
+api_version = 10
+
+--This let's the game know that this mod doesn't need to be listed in the server's mod listing
+client_only_mod = true
+
+--Let the mod system know that this mod is functional with Don't Starve Together
+dst_compatible = true
+
+all_clients_require_mod = false
+
+-- Can specify a custom icon for this mod!
+icon_atlas = "modIcon.xml"
+icon = "modIcon.tex"
+
+-- Specify the priority
+priority = 10
+
+configuration_options = {
+    {
+        name = "DG",
+        label = "Map Orientation",
+        options = {
+            { description = "0, North", data = 0 },
+            { description = "45, North-East", data = -45 },
+            { description = "90, East", data = -90 },
+            { description = "135, South-East", data = -135 },
+            { description = "180, South", data = -180 },
+            { description = "225, South-West", data = -225 },
+            { description = "270, West", data = -270 },
+            { description = "315, North-West", data = -315 },
+        },
+        default = 0
+    },
+    {
+        name = "CD",
+        label = "Show Character Direction on Map",
+        options = {
+            { description = "Yes", data = true },
+            { description = "No", data = false },
+        },
+        default = true
+    }
+}
---
+++ modmain.lua
@@ -0,0 +1,91 @@
+local Widget = require "widgets/widget"
+local Image = require "widgets/image"
+
+Assets = {
+	Asset("ATLAS", "images/arrow.xml"),
+}
+
+local previousRotation = 0
+local lcontrols = nil
+local button = nil
+local charPointer = nil
+local x_res = GLOBAL.RESOLUTION_X
+local y_res = GLOBAL.RESOLUTION_Y
+local w, h = GLOBAL.TheSim:GetScreenSize()
+
+--Map position is between -1 and 1 with a middle anchor
+local function MapPosToScreenPos(x, y)
+    return w * (x/2), h * (y/2)
+end
+
+local function AddArrow(mapscreen)
+    if charPointer ~= nil then charPointer:Kill() end
+    local zoom = GLOBAL.TheWorld.minimap.MiniMap:GetZoom()
+    w, h = GLOBAL.TheSim:GetScreenSize()
+    mapscreen.charPointer = mapscreen:AddChild(Image("images/arrow.xml", "arrow.tex"))
+    charPointer = mapscreen.charPointer
+    mapscreen.charPointer:SetRotation((GetModConfigData("DG") - previousRotation))
+    mapscreen.charPointer:SetScale(1.5/zoom, 1.5/zoom, 1)
+    mapscreen.charPointer:SetHAnchor(GLOBAL.ANCHOR_MIDDLE)
+    mapscreen.charPointer:SetVAnchor(GLOBAL.ANCHOR_MIDDLE)
+    mapscreen.charPointer:SetPosition(0, 0)
+    mapscreen.charPointer:MoveToFront()
+end
+
+local function RotateToZero(controls)
+    lcontrols = controls
+    local ToggleMap_base = controls.ToggleMap
+    controls.ToggleMap = function( self )
+        if not controls.owner.HUD:IsMapScreenOpen() then
+            previousRotation = GLOBAL.TheCamera:GetHeading()
+            ToggleMap_base(self)
+            GLOBAL.TheCamera:SetHeadingTarget(GetModConfigData("DG"))
+            GLOBAL.TheCamera:Snap()
+        else
+            GLOBAL.TheCamera:SetHeadingTarget(previousRotation)
+            GLOBAL.TheCamera:Snap()
+            --See map snap instead of character snap
+            --controls.inst:DoTaskInTime(0, function() ToggleMap_base(self) end)
+            ToggleMap_base(self)
+        end
+    end
+end
+
+AddClassPostConstruct( "widgets/controls", RotateToZero)
+if GetModConfigData("CD") then
+    AddClassPostConstruct( "screens/mapscreen", AddArrow)
+end
+
+local MapScreen = require "screens/mapscreen"
+
+MapScreen_OnControl_base = MapScreen.OnControl
+MapScreen.OnControl = function( self, control, down )
+	if not down and (control == GLOBAL.CONTROL_MAP or control == GLOBAL.CONTROL_CANCEL) then
+        GLOBAL.TheCamera:SetHeadingTarget(previousRotation)
+        GLOBAL.TheCamera:Snap()
+        --See map snap instead of character snap
+        --lcontrols.inst:DoTaskInTime(0, function() MapScreen_OnControl_base(self, control, down) end)
+        --return true
+	end
+    
+    local ret = MapScreen_OnControl_base(self, control, down)
+
+	return ret
+end
+
+--Update character direction arrow
+if GetModConfigData("CD") then
+    MapScreen_OnUpdate_base = MapScreen.OnUpdate
+    MapScreen.OnUpdate = function(self, dt)
+        MapScreen_OnUpdate_base(self, dt)
+        if charPointer ~= nil then
+            local ex,ey,ez = GLOBAL.ThePlayer.Transform:GetWorldPosition()
+            local x, y, z = self.minimap:WorldPosToMapPos(ex,ez,0)
+            local dx, dy = MapPosToScreenPos(x, y)
+            local zoom = GLOBAL.TheWorld.minimap.MiniMap:GetZoom()
+            charPointer:SetPosition(dx, dy)
+            charPointer:SetRotation(GLOBAL.TheCamera:GetHeading() - previousRotation)
+            charPointer:SetScale(1.5/zoom, 1.5/zoom, 1)
+        end
+    end
+end
